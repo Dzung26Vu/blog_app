848e4b61c756ae75d6ce50ffc09ba80c2b398055
Dzung26Vu <122276504+Dzung26Vu@users.noreply.github.com>
2024-06-12 07:58:31 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,8 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'https://blog-app-rose-five.vercel.app/'}));
+app.use(cors({credentials:true, origin:'https://blog-app-rose-five.vercel.app'}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
